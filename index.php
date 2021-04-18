db75c57fc362a9498143ca2b43794c2d398cc93e
yooerizkilab <yooerizki10@webmail.umm.ac.id>
2021-04-18 13:54:29 +0700

    test ssh

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Pages | Utama</title>
+</head>
+
+<body>
+    <!-- navbar -->
+    <div class="topnav">
+        <a href="">Profile</a>
+        <a href="">News</a>
+        <a class="active" href="">Home</a>
+    </div>
+    <!-- /navbar -->
+
+    <!-- sidenavbar -->
+    <section>
+        <div class="sidebar-right">
+            <p>kanan</p>
+        </div>
+
+        <div class="sidebar-left">
+            <p>kiri</p>
+        </div>
+    </section>
+    <!-- /sidenavbar -->
+
+    <!-- footer 
+    <div class="footer">
+        <p>Footer</p>
+    </div>
+    <!-- /footer -->
+</body>
+
+</html>
